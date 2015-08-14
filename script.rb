06b1dcc9712fa1064fc12b276f1349602d6c7783
malida <malida310892@gmail.com>
2015-08-14 12:05:09 -0500

    zweite

--- script.rb
+++ script.rb
@@ -5,4 +5,6 @@ project_root = File.expand_path(File.dirname(__FILE__))
 assets = Sprockets::Environment.new(project_root) do |env|
   env.register_bundle_processor 'application/javascript', Sprockets::JSMincCompressor.new
 end
+assets.append_path(File.join(project_root, 'javascript'))
 
+puts assets.find_asset('application.js')
